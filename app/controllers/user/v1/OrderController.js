62a4bf7c3d273ea246342e0200337da1df1e6f9d
mjh99 <mohmadjavadheydari9@gmail.com>
2021-10-19 18:38:50 +0330

    updated addOrderPush OrderController user v1

--- app/controllers/user/v1/OrderController.js
+++ app/controllers/user/v1/OrderController.js
@@ -1421,14 +1421,29 @@ module.exports = new class OrderController extends Controller {
             req.checkBody('distinationCall', 'please set distinationCall').notEmpty().isNumeric()
             if (this.showValidationErrors(req, res)) return;
 
-            let filter = { mobile: req.body.distinationCall, 'employeeVoipNumbers.voipNumber': req.body.voipId }
-            let user = await this.model.User.findOne(filter, {'employeeVoipNumbers.$.employeeId': 1}).lean()
+
+            if(req.body.distinationCall)
+            req.body.distinationCall = parseInt(req.body.distinationCall)
+
+            let filter =  { voipNumbers : req.body.distinationCall , 'employeeVoipNumbers.voipNumber': req.body.voipId } 
+    
+            let user = await this.model.User.findOne(filter).lean()
+
+
+            if (!user)
+                return res.json({ success: false, message: "کاربری با این شماره  ویپ وجود ندارد" });
+
+
+            let employee = user.employeeVoipNumbers.filter((item ) => item.voipNumber  ==  parseInt(req.body.voipId)  )
+
+            if (employee.length  === 0)
+                return res.json({ success: false, message: "کارمندی با این Sip  وجود ندارد" });
 
             let params = {
                 "projectId": "3",
                 "apiKey": "turboAABMoh",
                 "isImportant": "1",
-                "userId": user.employeeVoipNumbers[0].employeeId,
+                "userId": employee[0].employeeId,
                 "ttl": "100",
                 "message": "test"
             }
